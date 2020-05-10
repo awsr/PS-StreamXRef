0cb69b43924f5490a14149f744eaeb55b26375fa
awsr <43862868+awsr@users.noreply.github.com>
2020-05-10 03:35:23 -0700

    Create Clear-TXRConfig.ps1
    Initial untested version

---
+++ src/Clear-TXRConfig.ps1
@@ -0,0 +1,128 @@
+
+function Clear-TXRConfig {
+    [CmdletBinding(SupportsShouldProcess, ConfirmImpact = "Low", DefaultParameterSetName = "All")]
+    Param(
+        [Parameter(ParameterSetName = "All")]
+        [switch]$ResetAll = $false,
+
+        [Parameter(ParameterSetName = "Selection")]
+        [switch]$ApiKey = $false,
+
+        [Parameter(ParameterSetName = "Selection")]
+        [switch]$UserIdCache = $false,
+
+        [Parameter(ParameterSetName = "Selection")]
+        [switch]$ClipInfoCache = $false,
+
+        [Parameter(ParameterSetName = "Selection")]
+        [switch]$VideoStartCache = $false
+    )
+
+    DynamicParam {
+        if ($PSBoundParameters.ContainsKey("VideoStartCache")) {
+            $psnAttr = [System.Management.Automation.ParameterAttribute]::new()
+            $psnAttr.ParameterSetName = "Selection"
+            $vnnoeAttr = [System.Management.Automation.ValidateNotNullOrEmptyAttribute]::new()
+            $valScriptAttr = [System.Management.Automation.ValidateScriptAttribute]::new({ $_ -ge 0 })
+            $attributeCollection = [System.Collections.ObjectModel.Collection[System.Attribute]]::new()
+            $attributeCollection.Add($psnAttr)
+            $attributeCollection.Add($vnnoeAttr)
+            $attributeCollection.Add($valScriptAttr)
+    
+            $dynParam2Keep = [System.Management.Automation.RuntimeDefinedParameter]::new("DaysToKeep", [int], $attributeCollection)
+    
+            $paramDictionary = [System.Management.Automation.RuntimeDefinedParameterDictionary]::new()
+            $paramDictionary.Add("DaysToKeep", $dynParam2Keep)
+            return $paramDictionary
+        }
+    }
+    
+    Process {
+
+        if ($ResetAll) {
+
+            if ($PSCmdlet.ShouldProcess("All cached lookup data", "Delete")) {
+
+                $script:TwitchData.ApiKey = $null
+                $script:TwitchData.UserIdCache.Clear()
+                $script:TwitchData.ClipInfoCache.Clear()
+                $script:TwitchData.VideoStartCache.Clear()
+
+            }
+
+        }
+        elseif ($PSCmdlet.ParameterSetName -eq "Selection") {
+
+            if ($ApiKey) {
+
+                if ($PSCmdlet.ShouldProcess("ApiKey", "Delete")) {
+
+                    $script:TwitchData.ApiKey.Clear()
+                    Write-Verbose "API Key cleared"
+
+                }
+
+            }
+
+            if ($UserIdCache) {
+
+                if ($PSCmdlet.ShouldProcess("User ID lookup data", "Delete")) {
+
+                    $script:TwitchData.UserIdCache.Clear()
+                    Write-Verbose "User ID cache entries cleared"
+
+                }
+
+            }
+
+            if ($ClipInfoCache) {
+
+                if ($PSCmdlet.ShouldProcess("Clip info lookup data", "Delete")) {
+
+                    $script:TwitchData.ClipInfoCache.Clear()
+                    Write-Verbose "Clip cache entries cleared"
+
+                }
+
+            }
+
+            if ($VideoStartCache) {
+
+                if ($PSBoundParameters.ContainsKey("DaysToKeep")) {
+
+                    if ($PSCmdlet.ShouldProcess("Video timestamp lookup data", "Trim")) {
+
+                        $Cutoff = [datetime]::UtcNow - (New-TimeSpan -Days $DaysToKeep)
+    
+                        $PreviousVideoCacheCount = $script:TwitchData.VideoStartCache.Count
+        
+                        [string[]]$PurgeList = $script:TwitchData.VideoStartCache.GetEnumerator() |
+                            Where-Object { $_.Value -lt $Cutoff } | Select-Object -ExpandProperty Key
+            
+                        $PurgeList | ForEach-Object { $script:TwitchData.VideoStartCache.Remove($_) } | Out-Null
+            
+                        $NewVideoCacheCount = $script:TwitchData.VideoStartCache.Count
+            
+                        Write-Host "Video cache entries removed: $($PreviousVideoCacheCount - $NewVideoCacheCount)"
+
+                    }
+
+                }
+                else {
+    
+                    if ($PSCmdlet.ShouldProcess("Video timestamp lookup data", "Delete")) {
+    
+                        $script:TwitchData.VideoStartCache.Clear()
+                        Write-Verbose "Video cache entries cleared"
+    
+                    }
+    
+                }
+
+            }
+
+        }
+
+    }
+
+}
